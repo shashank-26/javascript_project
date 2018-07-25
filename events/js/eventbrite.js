0e9127f6b968d6c205426006ea8f74070d9115c8
shashank <shashankchaurasia97@gmail.com>
2018-07-25 16:35:32 +0530

    project-added

---
+++ events/js/eventbrite.js
@@ -0,0 +1,30 @@
+class EventBrite{
+
+    constructor(){
+        this.auth_token = '52L5SEVI7OGYKMHZXSUC';
+        this.orderby = 'date';
+    }
+
+    async queryAPI(eventName, category){
+        const eventsResponse = await fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${eventName}&sort_by=${this.orderby}&categories=${category}&token=${this.auth_token}`);
+
+        const events = await eventsResponse.json();    
+
+        return{
+            events
+        }
+    }
+
+    async getCategoriesAPI(){
+
+        const categoriesResponse = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${ this.auth_token}`);
+
+
+        const categories = await categoriesResponse.json();
+        return{
+            categories
+        }
+    }
+
+
+}
